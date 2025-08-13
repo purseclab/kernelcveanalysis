3d9ad19339423c981bad666d32acdc2a74bb41f2
Jack Roscoe <jack.x.roscoe@gmail.com>
2025-08-13 11:55:53 -0500

    split x86 exploit from cuttlefish exploit

--- bad_io_uring/compile_x86.sh
+++ bad_io_uring/compile_x86.sh
@@ -15,7 +15,7 @@ echo $CC
 cd "$(dirname $0)"
 
 # CC=gcc
-$CC exp_cuttlefish.c -o bad_io_uring
+# $CC exp_cuttlefish.c -o bad_io_uring
 
-# nix-shell -p glibc.static --run "gcc -static exp_cuttlefish.c -o bad_io_uring"
+nix-shell -p glibc.static --run "gcc -static exp_x86_64.c -o bad_io_uring"
 
