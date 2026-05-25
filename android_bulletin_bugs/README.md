44b306eb68a014b660ce36302b9446b231248e26
Jack Roscoe <jack.x.roscoe@gmail.com>
2026-05-25 04:16:22 -0500

    changes to advisory database

--- android_bulletin_bugs/README.md
+++ android_bulletin_bugs/README.md
@@ -4,3 +4,6 @@ Scrapes google android security bulletin for bugs and various info about them.
 
 In future querying info about the bugs, and perhaps info about selinux profiles and permissions granted by exploiting them.
 Depending on needs.
+
+Current datebase is updated up to may 2026 advisory. Note that 2026 advisories may not parse as well
+TODO: look into that
--- android_bulletin_bugs/src/bug_tracker/main.py
+++ android_bulletin_bugs/src/bug_tracker/main.py
@@ -1,38 +1,42 @@
 import argparse
-import requests
-from pathlib import Path
+import sqlite3
 from dataclasses import dataclass
 from enum import StrEnum
+from pathlib import Path
 from typing import Optional
 
-import sqlite3
+import requests
 from bs4 import BeautifulSoup
 from bs4.element import PageElement
 from rich.console import Console
 from rich.table import Table
 
+
 class BugType(StrEnum):
-    Rce = 'RCE'
-    ElevationOfPrivilege = 'EoP'
-    InformationDisclosure = 'ID'
-    DenialOfService = 'DoS'
+    Rce = "RCE"
+    ElevationOfPrivilege = "EoP"
+    InformationDisclosure = "ID"
+    DenialOfService = "DoS"
+
 
 class BugSeverity(StrEnum):
-    Critical = 'Critical'
-    High = 'High'
-    Moderate = 'Moderate'
-    Low = 'Low'
+    Critical = "Critical"
+    High = "High"
+    Moderate = "Moderate"
+    Low = "Low"
+
 
 class BugCategory(StrEnum):
-    AndroidRuntime = 'Android Runtime'
-    Framework = 'Framework'
-    MediaFramework = 'Media framework'
-    System = 'System'
-    Kernel = 'Kernel'
-    KernelLts = 'Kernel LTS'
+    AndroidRuntime = "Android Runtime"
+    Framework = "Framework"
+    MediaFramework = "Media framework"
+    System = "System"
+    Kernel = "Kernel"
+    KernelLts = "Kernel LTS"
 
     def is_kernel(self) -> bool:
         return self == BugCategory.Kernel or self == BugCategory.KernelLts
+
 
 @dataclass
 class Bug:
@@ -48,37 +52,43 @@ class Bug:
     updated_versions: Optional[list[str]]
     security_bulletin: str
 
+    def is_after_security_bulletin(self, year: int, month: int) -> bool:
+        bug_year, bug_month = self.security_bulletin.split("-")
+        return (13 * year + month) < (13 * int(bug_year) + int(bug_month))
+
+
 def readable_bulletin(bulletin: str) -> str:
     months = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
     ]
 
-    year, month = bulletin.split('-')
+    year, month = bulletin.split("-")
 
-    return f'{year} {months[int(month) - 1]}'
+    return f"{year} {months[int(month) - 1]}"
+
 
 def print_bugs(bugs: list[Bug]):
-    table = Table(title='Android Bugs')
+    table = Table(title="Android Bugs")
 
-    table.add_column('CVE', style='red')
-    table.add_column('Category')
-    table.add_column('Affected Component')
-    table.add_column('Type')
-    table.add_column('Severity')
-    table.add_column('Affected Versions')
-    table.add_column('Patch Date')
-    table.add_column('Patch Link', style='dim blue')
+    table.add_column("CVE", style="red")
+    table.add_column("Category")
+    table.add_column("Affected Component")
+    table.add_column("Type")
+    table.add_column("Severity")
+    table.add_column("Affected Versions")
+    table.add_column("Patch Date")
+    table.add_column("Patch Link", style="dim blue")
 
     for bug in bugs:
         table.add_row(
@@ -87,15 +97,17 @@ def print_bugs(bugs: list[Bug]):
             bug.component,
             str(bug.bug_type),
             str(bug.severity),
-            '' if bug.updated_versions is None else ' '.join(bug.updated_versions),
+            "" if bug.updated_versions is None else " ".join(bug.updated_versions),
             readable_bulletin(bug.security_bulletin),
             bug.reference_url,
         )
-    
+
     console = Console()
     console.print(table)
 
+
 DB_NAME = "android_advisories.db"
+
 
 class Db:
     # path: Path
@@ -109,7 +121,7 @@ class Db:
 
     def cursor(self) -> sqlite3.Cursor:
         return self.db.cursor()
-    
+
     def commit(self):
         self.db.commit()
 
@@ -130,24 +142,41 @@ class Db:
             )
         """)
         self.commit()
-    
+
     def add_bugs(self, bugs: list[Bug]):
         cur = self.cursor()
 
         sql_input = [
-            (bug.cve, bug.reference, bug.reference_url, str(bug.bug_type), str(bug.severity), str(bug.category), bug.component, None if bug.updated_versions is None else ','.join(bug.updated_versions), bug.security_bulletin)
+            (
+                bug.cve,
+                bug.reference,
+                bug.reference_url,
+                str(bug.bug_type),
+                str(bug.severity),
+                str(bug.category),
+                bug.component,
+                None
+                if bug.updated_versions is None
+                else ",".join(bug.updated_versions),
+                bug.security_bulletin,
+            )
             for bug in bugs
         ]
-        cur.executemany("""
+        cur.executemany(
+            """
             INSERT OR REPLACE INTO advisories (cve, reference, reference_url, bug_type, severity, category, component, updated_versions, bulletin)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-        """, sql_input)
-        
+        """,
+            sql_input,
+        )
+
         self.commit()
-    
+
     def get_bugs(self) -> list[Bug]:
         cur = self.cursor()
-        cur.execute("SELECT id, cve, reference, reference_url, bug_type, severity, category, component, updated_versions, bulletin FROM advisories ORDER BY component")
+        cur.execute(
+            "SELECT id, cve, reference, reference_url, bug_type, severity, category, component, updated_versions, bulletin FROM advisories ORDER BY component"
+        )
         rows = cur.fetchall()
 
         return [
@@ -160,70 +189,74 @@ class Db:
                 severity=BugSeverity(row[5]),
                 category=BugCategory(row[6]),
                 component=row[7],
-                updated_versions=None if row[8] is None else row[8].split(','),
+                updated_versions=None if row[8] is None else row[8].split(","),
                 security_bulletin=row[9],
-            ) for row in rows
+            )
+            for row in rows
         ]
 
 
 def classify_bug_component(patch_url: str) -> str:
-    path_parts = patch_url.split('/')[3:-2]
-    path = '/'.join(path_parts)
+    path_parts = patch_url.split("/")[3:-2]
+    path = "/".join(path_parts)
 
     components = {
-        'platform/packages/modules/Bluetooth': 'bluetooth',
+        "platform/packages/modules/Bluetooth": "bluetooth",
         # old bluetooth repo
-        'platform/system/bt': 'bluetooth',
-        'platform/packages/modules/Wifi': 'wifi',
+        "platform/system/bt": "bluetooth",
+        "platform/packages/modules/Wifi": "wifi",
         # userspace libraries for networking cell data and other stuff like dhcp
-        'platform/packages/modules/Connectivity': 'android_networking',
+        "platform/packages/modules/Connectivity": "android_networking",
         # old android networking path
-        'platform/frameworks/libs/net': 'android_networking',
+        "platform/frameworks/libs/net": "android_networking",
         # various audio and video code
-        'platform/frameworks/av': 'audio_video',
+        "platform/frameworks/av": "audio_video",
         # permission server and ui and managing app permissions
-        'platform/packages/modules/Permission': 'permissions',
+        "platform/packages/modules/Permission": "permissions",
         # handles which apps should be used for which intents, and shows ui to pick if 2 things are available
-        'platform/packages/modules/IntentResolver': 'IntentResolver',
+        "platform/packages/modules/IntentResolver": "IntentResolver",
         # statistics and telemetry about ths system
-        'platform/packages/modules/StatsD': 'StatsD',
-        'platform/packages/modules/HealthFitness': 'HealthFitness',
+        "platform/packages/modules/StatsD": "StatsD",
+        "platform/packages/modules/HealthFitness": "HealthFitness",
         # cryptography implementation of hal, hardware assisted keygen and cryptography
-        'platform/system/keymint': 'hal_crypto',
+        "platform/system/keymint": "hal_crypto",
         # manages calls, accepting them, voip, etc.
-        'platform/packages/services/Telecomm': 'telecomm',
-        'platform/packages/services/Telephony': 'Telephony',
-        'platform/packages/apps/Settings': 'Settings',
+        "platform/packages/services/Telecomm": "telecomm",
+        "platform/packages/services/Telephony": "Telephony",
+        "platform/packages/apps/Settings": "Settings",
         # documents ui is like the default android file picker that shows up when an app wants to open a file
-        'platform/packages/apps/DocumentsUI': 'DocumentsUI',
+        "platform/packages/apps/DocumentsUI": "DocumentsUI",
         # provisioning for enterprise applications
-        'platform/packages/apps/ManagedProvisioning': 'ManagedProvisioning',
+        "platform/packages/apps/ManagedProvisioning": "ManagedProvisioning",
         # something involved in downloading files
         # apps ask this service to download files in the background
-        'platform/packages/providers/DownloadProvider': 'DownloadProvider',
+        "platform/packages/providers/DownloadProvider": "DownloadProvider",
         # manages photos, videos, stored downloads, and permissions for apps to access them
-        'platform/packages/providers/MediaProvider': 'MediaProvider',
-        'platform/hardware/st/nfc': 'nfc',
+        "platform/packages/providers/MediaProvider": "MediaProvider",
+        "platform/hardware/st/nfc": "nfc",
         # adobe image format library
-        'platform/external/dng_sdk': 'dng_sdk',
+        "platform/external/dng_sdk": "dng_sdk",
         # for GIF images
-        'platform/external/giflib': 'giflib',
+        "platform/external/giflib": "giflib",
         # java cryptography library
-        'platform/external/conscrypt': 'conscrypt',
+        "platform/external/conscrypt": "conscrypt",
         # 2d drawing library
-        'platform/external/skia': 'skia',
+        "platform/external/skia": "skia",
         # font format parsing and rendering library
-        'platform/external/freetype': 'freetype',
+        "platform/external/freetype": "freetype",
         # core android libraries
         # TODO: be more specific
-        'platform/system/core': 'core',
+        "platform/system/core": "core",
         # java standard library implentation for android
-        'platform/libcore': 'java_std',
+        "platform/libcore": "java_std",
     }
 
-    return components.get(path, path.split('/')[-1])
+    return components.get(path, path.split("/")[-1])
 
-def get_bugs_from_table(table: PageElement, bulletin: str, category: BugCategory, is_kernel: bool = False) -> list[Bug]:
+
+def get_bugs_from_table(
+    table: PageElement, bulletin: str, category: BugCategory, is_kernel: bool = False
+) -> list[Bug]:
     rows = table.find_all("tr")[1:]  # skip header row
     advisories = []
 
@@ -241,19 +274,17 @@ def get_bugs_from_table(table: PageElement, bulletin: str, category: BugCategory
         bug_type = BugType(cols[2].get_text(strip=True))
         severity = BugSeverity(cols[3].get_text(strip=True))
         updated_versions = cols[4].get_text(strip=True).split()
-        updated_versions = [
-            version.strip(',') for version in updated_versions
-        ]
+        updated_versions = [version.strip(",") for version in updated_versions]
 
         if is_kernel:
             component = cols[4].get_text(strip=True)
             updated_versions = None
         else:
-            component = '' if reference_url is None else classify_bug_component(reference_url)
+            component = (
+                "" if reference_url is None else classify_bug_component(reference_url)
+            )
             updated_versions = cols[4].get_text(strip=True).split()
-            updated_versions = [
-                version.strip(',') for version in updated_versions
-            ]
+            updated_versions = [version.strip(",") for version in updated_versions]
 
         bug = Bug(
             id=0,
@@ -269,33 +300,43 @@ def get_bugs_from_table(table: PageElement, bulletin: str, category: BugCategory
         )
 
         advisories.append(bug)
-    
+
     return advisories
 
+
 def get_named_bug_table(soup: BeautifulSoup, name: str) -> Optional[PageElement]:
-    element = soup.find('h3', attrs={"data-text": name})
+    element = soup.find("h3", attrs={"data-text": name})
     if element is None:
         return None
-    
-    return element.find_next('table')
+
+    return element.find_next("table")
+
 
 def import_advisories(db: Db, bulletin: str):
     """Fetch the advisories page, parse it, and insert entries into SQLite."""
 
-    url = f'https://source.android.com/docs/security/bulletin/{bulletin}-01'
+    year, _ = bulletin.split("-")
+    if int(year) >= 2026:
+        url = f"https://source.android.com/docs/security/bulletin/{year}/{bulletin}-01"
+    else:
+        url = f"https://source.android.com/docs/security/bulletin/{bulletin}-01"
 
     resp = requests.get(url)
     resp.raise_for_status()
 
     soup = BeautifulSoup(resp.text, "html.parser")
-    
+
     advisories = []
 
     for category in BugCategory:
         table = get_named_bug_table(soup, str(category))
 
         if table is not None:
-            advisories.extend(get_bugs_from_table(table, bulletin, category, is_kernel=category.is_kernel()))
+            advisories.extend(
+                get_bugs_from_table(
+                    table, bulletin, category, is_kernel=category.is_kernel()
+                )
+            )
 
     # Insert into DB with deduplication
     db.add_bugs(advisories)
@@ -304,13 +345,19 @@ def import_advisories(db: Db, bulletin: str):
 
     print_bugs(advisories)
 
+
 def reclassify_bugs(db: Db):
     bugs = db.get_bugs()
     for bug in bugs:
         if not bug.category.is_kernel():
-            bug.component = '' if bug.reference_url is None else classify_bug_component(bug.reference_url)
-    
+            bug.component = (
+                ""
+                if bug.reference_url is None
+                else classify_bug_component(bug.reference_url)
+            )
+
     db.add_bugs(bugs)
+
 
 class FilterSet:
     negative: bool
@@ -322,12 +369,12 @@ class FilterSet:
             self.filter_set = None
             return
 
-        if arg_value.startswith('!'):
+        if arg_value.startswith("!"):
             arg_value = arg_value[1:]
             self.negative = True
-        
-        self.filter_set = set(arg.lower() for arg in arg_value.split(','))
-    
+
+        self.filter_set = set(arg.lower() for arg in arg_value.split(","))
+
     def contains(self, value: str) -> bool:
         if self.filter_set is None:
             return True
@@ -336,6 +383,7 @@ class FilterSet:
         else:
             return value.lower() in self.filter_set
 
+
 def list_advisories(db: Db, args: argparse.Namespace):
     """Print stored advisories from SQLite."""
 
@@ -343,6 +391,16 @@ def list_advisories(db: Db, args: argparse.Namespace):
     components = FilterSet(args.component)
     types = FilterSet(args.type)
     severity = FilterSet(args.severity)
+
+    year = None
+    month = None
+    if args.after_date:
+        a, b = args.after_date.split("-")
+        year = int(a)
+        month = int(b)
+        if not (0 < month <= 12):
+            print("invalid --after-date month")
+            return
 
     bugs = []
     for bug in db.get_bugs():
@@ -354,27 +412,48 @@ def list_advisories(db: Db, args: argparse.Namespace):
         ):
             continue
 
+        if (
+            year is not None
+            and month is not None
+            and not bug.is_after_security_bulletin(year, month)
+        ):
+            continue
+
         bugs.append(bug)
-    
+
     print_bugs(bugs)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Manage Android advisories database.")
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     # import command
-    import_parser = subparsers.add_parser("import", help="Import advisories from a given URL")
-    import_parser.add_argument("url", help="URL of the Android advisory page")
+    import_parser = subparsers.add_parser(
+        "import", help="Import advisories from a given URL"
+    )
+    import_parser.add_argument("date", help="Date of android advisory (yyyy-mm format)")
 
     # reaclassify command
     subparsers.add_parser("reclassify", help="Reclassify bugs")
 
     # list command
     list_parser = subparsers.add_parser("list", help="List advisories in the database")
-    list_parser.add_argument("--category", type=str, help="Filter based on bug category")
-    list_parser.add_argument("--component", type=str, help="Filter based on bug affected component")
+    list_parser.add_argument(
+        "--category", type=str, help="Filter based on bug category"
+    )
+    list_parser.add_argument(
+        "--component", type=str, help="Filter based on bug affected component"
+    )
     list_parser.add_argument("--type", type=str, help="Filter based on bug type")
-    list_parser.add_argument("--severity", type=str, help="Filter based on bug severity")
+    list_parser.add_argument(
+        "--severity", type=str, help="Filter based on bug severity"
+    )
+    list_parser.add_argument(
+        "--after-date",
+        type=str,
+        help="Filter based on patches after a given date (yyyy-mm format)",
+    )
 
     args = parser.parse_args()
 
@@ -382,13 +461,14 @@ def main():
     db.init_db()
 
     if args.command == "import":
-        import_advisories(db, args.url)
+        import_advisories(db, args.date)
     elif args.command == "reclassify":
         reclassify_bugs(db)
     elif args.command == "list":
         list_advisories(db, args)
 
     db.close()
+
 
 if __name__ == "__main__":
     main()
